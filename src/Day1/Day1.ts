629effc54b16ee5e345c9a6b22cdb89ee164de0a
Henrik Carlioth <hca@qlik.com>
2016-12-23 13:41:40 +0100

    added day1 part1 solution

--- src/Day1/Day1.ts
+++ src/Day1/Day1.ts
@@ -1,2 +1,19 @@
 import * as inpus from "./input";
 
+function GoUpOrDown(dir: string, counter: number): number {
+    if (dir == '(')
+        return ++counter;
+    return --counter;
+}
+
+function read(): void {
+    const allIinstructions: string[] = inpus.input.split('');
+    let level: number = 0;
+
+    for(let instr of allIinstructions) {
+        level = GoUpOrDown(instr, level)
+    }
+    console.log(level);
+}
+
+read();
