da03333875b165799b15e10cbff52d4a917b1f8b
Henrik Carlioth <hca@qlik.com>
2016-12-28 16:04:09 +0100

    moving assets

--- src/Day2/Day2.ts
+++ src/Day2/Day2.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 
 
 function getInput(): Array<string> {
-    let input: Array<string> = file.readFileSync(path.join(__dirname, "input.txt")).toString().split("\r\n");
+    let input: Array<string> = file.readFileSync("dist/input/day2.txt").toString().split("\r\n");
 input.forEach(word => console.log(word));
 return input;
 }
